173be371cc376ec081653b732485e3f20f0b9191
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-05-03 00:49:39 -0300

    basics of firebase usage

--- App.js
+++ App.js
@@ -1,26 +1,70 @@
 import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 import firebase from 'firebase';
 
 export default class App extends Component {
+    constructor(props){
+    	super(props);
+    	this.state = {
+            pontuacao: 0
+        };
+    }
+
+
   componentWillMount() {
     const config = {
-      apiKey: "AIzaSyAHPKRPCRKh4l-lt0ThLcXQ_r6TAlODmV0",
-      authDomain: "fir-react-teste.firebaseapp.com",
-      databaseURL: "https://react-native-realtime-chat.firebaseio.com",
-      projectId: "fir-react-teste",
-      storageBucket: "fir-react-teste.appspot.com",
-      messagingSenderId: "396556937415"
+        apiKey: "AIzaSyAHPKRPCRKh4l-lt0ThLcXQ_r6TAlODmV0",
+        authDomain: "fir-react-teste.firebaseapp.com",
+        databaseURL: "https://fir-react-teste.firebaseio.com",
+        projectId: "fir-react-teste",
+        storageBucket: "fir-react-teste.appspot.com",
+        messagingSenderId: "396556937415"
     };
     firebase.initializeApp(config);
   }
 
+  saveData() {
+      var employers  = firebase.database().ref('employers');
+      employers.remove();
+      employers.push().child('name').set('William');
+      employers.push().child('name').set('Set');
+      employers.push().child('name').set('Set');
+      employers.push().set({
+          name: "Fabio"
+      });
+
+      var pontuacao = firebase.database().ref('pontuacao');
+      pontuacao.set(100);
+      employers.push().remove();
+  }
+
+  listData() {
+      const pontuacao = firebase.database().ref('pontuacao');
+      pontuacao.on('value', (snapshot) => {
+          const pontuacao = snapshot.val();
+          this.setState({ pontuacao });
+      });
+  }
+
   render() {
+      const { pontuacao } = this.state;
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Hello World</Text>
+          <Button
+              onPress={ () => { this.saveData() }}
+              title="Save data"
+              color="#841584"
+              accessibilyLabel="Save data"
+          />
+          <Button
+              onPress={ () => { this.listData() }}
+              title="List data"
+              color="#841584"
+              accessibilyLabel="List data"
+          />
+      <Text>{pontuacao}</Text>
       </View>
     );
   }
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# react-native-firebase
+# React Native Firebase Example
+
+A react native app using firebase as example.
+
+Feel free to use the firebase existing or replace with yours.
+
+# Installation
+
+npm run install
+
+# Running the project
+
+react-native run-ios
+
+OR
+
+react-native run-android
